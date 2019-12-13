a3ce65b522ea7a5c8a5b2e77663fd3c44e2e1256
gozdeylmz95 <gozdeylmz95@gmail.com>
2019-12-13 17:03:48 +0300

    Updates

---
+++ precache-manifest.3ee37d9eb0bab52e3d62128657f296d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eacd836a485315924fd2be7aa4e9708b",
+    "url": "/Pets-App/index.html"
+  },
+  {
+    "revision": "bffa88537ccda336abe5",
+    "url": "/Pets-App/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "92b80eef5e0f723c484d",
+    "url": "/Pets-App/static/css/main.613c09ec.chunk.css"
+  },
+  {
+    "revision": "bffa88537ccda336abe5",
+    "url": "/Pets-App/static/js/2.c9bf0c0b.chunk.js"
+  },
+  {
+    "revision": "92b80eef5e0f723c484d",
+    "url": "/Pets-App/static/js/main.d21bfc22.chunk.js"
+  },
+  {
+    "revision": "2637c84b5c04b94c813c",
+    "url": "/Pets-App/static/js/runtime-main.e1e22e64.js"
+  }
+]);
